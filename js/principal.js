0738a36dbd8b55252bac6c84c550507c033f063b
Lucas adams <adams.lucass@gmail.com>
2017-06-16 02:53:32 -0300

    modulo 3

--- js/principal.js
+++ js/principal.js
@@ -1,32 +1,42 @@
+// Titulo da aplicacao
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
+// Calculo de IMC
+var pacientes = document.querySelectorAll('.paciente');
 
-var tdImc = paciente.querySelector(".info-imc");
+for (var i = 0; i < pacientes.length; i++) {
+    var tdPeso = pacientes[i].querySelector(".info-peso");
+    var tdAltura = pacientes[i].querySelector(".info-altura");
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+    var tdImc = pacientes[i].querySelector(".info-imc");
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
 
-if (peso <= 0 || peso >= 1000 ) {
-    console.log('Peso invalido');
-    tdImc.textContent = 'Peso invalido';
-    pesoEhValido = false;
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000 ) {
+        console.log('Peso invalido');
+        tdImc.textContent = 'Peso invalido';
+        pesoEhValido = false;
+
+        pacientes[i].classList.add('paciente-invalido');
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log('Altura invalida');
+        tdImc.textContent = 'Altura invalida';
+        alturaEhValida = false;
+
+        pacientes[i].classList.add('paciente-invalido');
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso/(altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
 
-if (altura <= 0 || altura >= 3.00) {
-    console.log('Altura invalida');
-    tdImc.textContent = 'Altura invalida';
-    alturaEhValida = false;
-}
-
-if (alturaEhValida && pesoEhValido) {
-    var imc = peso/(altura * altura);
-    tdImc.textContent = imc;
-}
 
